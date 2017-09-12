d35f7e16c8c1b61a503e8cf0585246df90e51148
zhangwei8387 <zhangwei8387@163.com>
2017-09-12 15:55:31 +0800

    提交注释
    提交注释

--- index.js
+++ index.js
@@ -1,41 +1,52 @@
+// 引入puppeteer和restify
 const puppeteer = require('puppeteer');
 const restify = require('restify');
-const ip_addr = '127.0.0.1';
+
+//指定Server的IP地址和端口
+const ip_addr = '127.0.0.1'; 
 const port = '8080';
 
+//启动Server
 var server = restify.createServer({
-  name: 'printapi'
+  name: 'Batch-Print-Server'
 });
 
 server.listen(port, ip_addr, function() {
   console.log('%s listening at %s ', server.name, server.url);
 });
 
-PATH = '/print';
+//启用restify的插件
 server.use(restify.plugins.queryParser());
 server.use(restify.plugins.bodyParser({ requestBodyOnGet: true }));
+
+//指定Route
+PATH = '/print';
+//指定相应Route的方法
 server.post({ path: PATH, version: '0.0.1' }, postNewPrintJob);
 
 function postNewPrintJob(req, res, next) {
-  console.log(req.body);
-
   var job = {};
   //    job.title = req.params.title;
   //    job.description = req.params.description;
   job.location = req.params.location;
   job.filename = req.params.filename;
-  job.postedOn = new Date();
+  job.postedOn = new Date().toLocaleString();
 
   console.log(job);
+
+  //设置跨域响应
   res.setHeader('Access-Control-Allow-Origin', '*');
 
   (async () => {
-    console.log(job);
+    //启动Chrome
     const browser = await puppeteer.launch();
+    //新建Page
     const page = await browser.newPage();
-
+    //打开页面 load方式指页面所有事务执行完毕
     await page.goto(job.location, { waitUntil: 'load' });
+    //指定响应Media为Print
     await page.emulateMedia('print');
+    //生成PDF文件，format设为A4,margin为默认页边距。
     const pdffile = await page.pdf({
       path: job.filename,
       format: 'A4',
@@ -46,11 +57,11 @@ function postNewPrintJob(req, res, next) {
         right: '10mm'
       }
     });
-
+    //关闭浏览器。
     browser.close();
 
     if (pdffile) {
-       
+    //返回结果。
       res.send(200, pdffile);
       return next();
     }
